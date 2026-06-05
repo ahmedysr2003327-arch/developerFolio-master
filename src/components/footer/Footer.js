11865d38234db608c55992ebf7ec5a57358f2097
Ahmed Yaser <ahmedysr777@gmail.com>
2026-06-05 10:11:26 +0300

    my githup link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a
-            href="https://github.com/saadpasta/developerFolio"
+            href="https://github.com/ahmedysr2003327-arch?tab=repositories"
             target="_blank"
             rel="noreferrer"
           >
